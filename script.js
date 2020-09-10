8e44562b93c8944645158d951b03954b839a79ce
oscarcalzadadev <sendoscaramessage@gmail.com>
2020-09-10 16:46:44 -0500

    updated files

---
+++ script.js
@@ -0,0 +1,18 @@
+/*When user clicks on a game square, an x or o appears depending on who's turn it is */
+//This is a two player game
+//players take turns
+//one player is assigned the value of x  & the other is assigned the value of O
+//The current turn decides whos turn it is and what letter they use 
+let turn;
+let currentPlayer;
+let player1 = "Jake";
+let player2 = "Chrissy";
+
+
+
+/*OOJS VERSION*/
+/*
+let game = {
+    turn: 0,
+    currentPlayer: player1,
+}*/
